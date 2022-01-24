b543680992d6d7e2329e721d7094babcf759b732
Patricia <patyamiga16@hotmail.com>
2022-01-24 01:18:17 -0300

    categorias

--- src/main/java/br/com/alura/loja2/Dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja2/Dao/ProdutoDao.java
@@ -1,5 +1,7 @@
 package br.com.alura.loja2.Dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.alura.loja2.modelo.Produto;
@@ -15,5 +17,37 @@ public class ProdutoDao {
 
 	public void cadastrar(Produto produto) {
 		this.em.persist(produto);
+	}
+
+	public void atualizar(Produto produto) {
+		this.em.persist(produto);
+	}
+
+	public void remover(Produto produto) {
+		produto = em.merge(produto);
+		this.em.persist(produto);
+	}
+
+	public Produto buscarPorId(Long id) {
+		return em.find(Produto.class, id);
+
+	}
+
+	public List<Produto> buscarTodos() {
+		String jpql = "SELECT p FROM Produto p";
+		return em.createQuery(jpql, Produto.class).getResultList();
+
+	}
+
+	public List<Produto> buscarPorNome(String nome) {
+		String jpql = "SELECT p FROM Produto p WHERE p.nome = : nome";
+		return em.createQuery(jpql, Produto.class).setParameter("nome", nome).getResultList();
+
+	}
+
+	public List<Produto> buscarPorNomeDaCategoria(String nome) {
+		String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = : nome";
+		return em.createQuery(jpql, Produto.class).setParameter("nome", nome).getResultList();
+
 	}
 }
--- src/main/java/br/com/alura/loja2/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja2/testes/CadastroDeProduto.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja2.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -14,22 +15,32 @@ public class CadastroDeProduto {
 
 	public static void main(String[] args) {
 
+		cadastrarProduto();
+		Long id = 1l;
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(em);
+
+		Produto p = produtoDao.buscarPorId(1l);
+		System.out.println(p.getPreco());
+
+		List<Produto> todos = produtoDao.buscarPorNomeDaCategoria("CELULARES");
+		todos.forEach(p2 -> System.out.println(p.getNome()));
+	}
+
+	private static void cadastrarProduto() {
 		Categoria celulares = new Categoria("CELULARES");
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
 
 		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
+
 		em.getTransaction().begin();
 
-		em.persist(celulares);
-		celulares.setNome("XPTO");
-		
-		em.flush();
-		em.clear();
-		
-		celulares = em.merge(celulares);
-		celulares.setNome("1234");
-		em.flush();	
-		em.clear();
-		em.remove(celulares);
-		em.flush();
+		categoriaDao.cadastrar(celulares);
+		produtoDao.cadastrar(celular);
+
+		em.getTransaction().commit();
+		em.close();
 	}
 }
