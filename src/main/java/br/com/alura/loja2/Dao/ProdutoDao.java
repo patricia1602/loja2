d737eb3e13364962515cfde10c94e3255a5717d4
Patricia <patyamiga16@hotmail.com>
2022-01-28 00:26:18 -0300

    produto

--- src/main/java/br/com/alura/loja2/Dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja2/Dao/ProdutoDao.java
@@ -47,13 +47,12 @@ public class ProdutoDao {
 	}
 
 	public List<Produto> buscarPorNomeDaCategoria(String nome) {
-		String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = : nome";
-		return em.createQuery(jpql, Produto.class).setParameter("nome", nome).getResultList();
+		return em.createNamedQuery("Produto.produtosPorCategoria", Produto.class).setParameter("nome", nome).getResultList();
 
 	}
 
 	public BigDecimal buscarPrecoDoProdutoComNome(String nome) {
-		String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = : nome";
+		String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
 		return em.createQuery(jpql, BigDecimal.class).setParameter("nome", nome).getSingleResult();
 
 	}
