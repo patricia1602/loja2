e3e94023546eb63fd1da6a44dab711b870fb1fe6
Patricia <patyamiga16@hotmail.com>
2022-02-16 11:36:22 -0300

    produto

--- src/main/java/br/com/alura/loja2/Dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja2/Dao/ProdutoDao.java
@@ -1,9 +1,11 @@
 package br.com.alura.loja2.Dao;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import br.com.alura.loja2.modelo.Produto;
 
@@ -47,7 +49,8 @@ public class ProdutoDao {
 	}
 
 	public List<Produto> buscarPorNomeDaCategoria(String nome) {
-		return em.createNamedQuery("Produto.produtosPorCategoria", Produto.class).setParameter("nome", nome).getResultList();
+		return em.createNamedQuery("Produto.produtosPorCategoria", Produto.class).setParameter("nome", nome)
+				.getResultList();
 
 	}
 
@@ -55,5 +58,32 @@ public class ProdutoDao {
 		String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
 		return em.createQuery(jpql, BigDecimal.class).setParameter("nome", nome).getSingleResult();
 
+	}
+
+	public List<Produto> buscarPorParametros(String nome, BigDecimal preco, LocalDate dataCadastro) {
+		String jpql = "SELECT p FROM Produto p WHERE 1=1";
+		if (nome != null && !nome.trim().isEmpty()) {
+			jpql = "WHERE p.nome = :nome";
+		}
+		if (preco != null) {
+			jpql = "AND p.preco = :preco";
+		}
+		if (dataCadastro != null) {
+			jpql = "AND p.dataCadastro = :dataCadastro";
+
+			TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
+			if (nome != null && !nome.trim().isEmpty()) {
+				query.setParameter("nome", nome);
+			}
+			if (preco != null) {
+				query.setParameter("preco", preco);
+			}
+			if (dataCadastro != null) {
+				query.setParameter("dataCadastro", dataCadastro);
+
+			}
+
+			return query.getResultList();
+		}
 	}
 }
